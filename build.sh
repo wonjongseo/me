ee4b2ff6ae9fe6fd636521804aa7995e669e94bf
Jongseo Won <jongseowon@Jongseos-MacBook-Pro.local>
2024-04-22 22:58:17 +0900

    add build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+if cd flutter; then
+  git pull
+  cd ..
+else
+  git clone https://github.com/flutter/flutter.git
+fi
+
+flutter/bin/flutter config --enable-web
+
+flutter/bin/flutter build web --release
